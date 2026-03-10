9d22401497d47d3985f660d96311d73125d53bf6
arifashraf717-pixel <arifashraf717@gmail.com>
2026-03-10 15:15:45 +0530

    frontend2

--- backend/api_demo.py
+++ backend/api_demo.py
@@ -6,15 +6,14 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Agri AI Backend Running"}
+    return {"message":"Agri AI system running"}
 
 @app.post("/detect")
-
 async def detect(file: UploadFile = File(...)):
 
     path = "temp.jpg"
 
-    with open(path, "wb") as buffer:
+    with open(path,"wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     result = detect_image(path)
